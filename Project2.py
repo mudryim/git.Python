938f80aa090ca33682b6f0010e61c71a695f76f0
Your Name <you@example.com>
2022-06-23 07:48:31 +0300

    update Project2

--- Project2.py
+++ Project2.py
@@ -1,4 +1,6 @@
 #NoteBook of your car
+import math
+#Start
 print("Хай! Давай запишем историю твоей ласточки")
 info = {}
 info2 = {}
@@ -10,11 +12,13 @@ km = input('Введите пробег: ')
 info[dat] = km
 #user input values of fixing car
 while active:
+    #Repeat noting
     fix = input('ТО, ремонт: ')
     cost = int(input('Сумма грн.:'))
-    info2[fix] = cost
     cos_list.append(cost)
+    info2[fix] = cost
     respond = input('Завершть запись? (press "q")')
+    #Interrupt note
     if respond == "q":
         active = False
 # showing results
@@ -23,5 +27,7 @@ for dat, km  in info.items():
     print(f"\tДата: {dat}, Пробег: {km}")
 for fix, cost in info2.items():
     print(f"\nТО, Ремонт: {fix}\nСумма: {cost}")
+#Total sum
+print("Общая сумма: ", sum(cos_list))
 
 input('')
